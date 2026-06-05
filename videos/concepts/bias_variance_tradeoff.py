afc73a22af92e13b9834626c179724724f3459ed
Gabriel Menezes <dmenezes.gabriel@gmail.com>
2026-06-05 12:08:37 -0300

    chore(videos): remove old flat concept files
    Replaced by package-based layout under videos/src/.

--- videos/concepts/bias_variance_tradeoff.py
+++
@@ -1,8 +0,0 @@
-from manim import Scene, Text
-
-
-class BiasVarianceTradeoff(Scene):
-    def construct(self) -> None:
-        title = Text("Bias-Variance Tradeoff")
-        self.play(title.animate.scale(1.1))
-        self.wait(1)
