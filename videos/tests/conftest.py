21eccee2b34af9b7bd05fd8d92c57d05e573e072
Gabriel Menezes <dmenezes.gabriel@gmail.com>
2026-06-05 12:47:08 -0300

    Remove old videos package at root (moved to libs/videos/)

--- videos/tests/conftest.py
+++
@@ -1,7 +0,0 @@
-import pytest
-from mlops_videos.concepts._registry import ConceptRegistry
-
-
-@pytest.fixture(autouse=True)
-def _clear_registry() -> None:
-    ConceptRegistry._extensions.clear()
